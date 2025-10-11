8985f541f0cf1b1927217eaa7b2c243786e9e535
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-12 00:14:16 +0300

    fix error in php

--- admin/index.php
+++ admin/index.php
@@ -7,4 +7,4 @@ add_action('admin_enqueue_scripts', function(){
     wp_enqueue_script('admin-script', plugin_dir_url(__FILE__).'assets/js/admin.js');
 });
 
-require plugin_dir_url(__FILE__) . 'columns.php';
+require plugin_dir_path(__FILE__) . 'columns.php';
