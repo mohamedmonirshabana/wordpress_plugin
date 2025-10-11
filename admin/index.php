613cdb949e80f82e0f85dae4bfeb2b97c5bef538
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-11 23:26:34 +0300

    add fontawesome manin library

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php 
 
 add_action('admin_enqueue_scripts', function(){
+    wp_enqueue_style('fontawesome-icons', get_template_directory_uri().'assets/css/font-awesome.min.css');
     wp_enqueue_style('iconpicker-style',plugin_dir_url(__FILE__).'assets/css/fontawesome-iconpicker.min.css');
     wp_enqueue_script('iconpicker-script', plugin_dir_url(__FILE__).'assets/js/fontawesome-iconpicker.min.js');
     wp_enqueue_script('admin-script', plugin_dir_url(__FILE__).'assets/js/admin.js');
