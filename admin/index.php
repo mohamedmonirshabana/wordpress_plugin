a05d7b1826a681e92759d92b7a84954e211c06e6
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-11 18:23:12 +0300

    add code to upload files

---
+++ admin/index.php
@@ -0,0 +1,6 @@
+<?php 
+
+add_action('admin_enqueue_scripts', function(){
+    wp_enqueue_style('iconpicker-style',plugin_dir_url(__FILE__).'assets/css/fontawesome-iconpicker.min.css');
+    wp_enqueue_script('iconpicker-script', plugin_dir_url(__FILE__).'assets/js/fontawesome-iconpicker.min.js');
+});
