38c87312aae32be6b81a60070863c464844e3cdb
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-12 20:51:15 +0300

    add submit for links user add it to social page

--- admin/custom-pages.php
+++ admin/custom-pages.php
@@ -34,18 +34,32 @@ if(!function_exists('wpc_social_media_options')){
     function wpc_social_media_options()
     {
         $networks = ['facebook', 'flickr' ,'instagram','pintrest', 'twitter', 'youtube'];
+        if(isset($_POST['_wpnonce'])){
+            if(!wp_verify_nonce($_POST['_wpnonce'], 'wpc_social_options')){
+                wp_die();
+            }
+            $links = [];
+            foreach($networks as $network){
+                if(isset($_POST['wpc_'.$network.'_link'])){
+                    $links[$network] = esc_url_raw($_POST['wpc_'.$network.'_link']);
+                }
+            }
+            update_option('wpc_social_options',$links,false);
+        }
         ?>
         <div class="wrap">
             <h1><?php _e('Social Media Links', 'wpcourse'); ?></h1>
             <form action="" method="post">
                 <table class="form-table">
                     <?php
+                        $links = get_option('wpc_social_options');
                         foreach($networks as $network){
+                            $link = isset($links[$network]) ?  $links[$network] : '';
                             ?>
                                 <tr>
                                     <th><?php _e(ucfirst($network), 'wpcourse'); ?></th>
                                     <td>
-                                        <input type="url" name="<?php echo esc_attr('wpc_'.$network .'_link'); ?>" id="">
+                                        <input type="url" name="<?php echo esc_attr('wpc_'.$network .'_link'); ?>" value="<?php echo esc_url($link);  ?>">
                                     </td>
                                 </tr>
                             <?php
@@ -55,6 +69,7 @@ if(!function_exists('wpc_social_media_options')){
                 <p class="submit">
                     <input type="submit" value="<?php echo esc_attr(__('Save Social Links', 'wpcourse'))  ?>" class="button button-primary">
                 </p>
+                <?php wp_nonce_field('wpc_social_options'); ?>
             </form>
         </div>
         <?php
