ea42efa87cc831afb4f5472cb99cca05f8cf6f9b
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-12 17:15:11 +0300

    add if to check theme support thumbnail

--- admin/columns.php
+++ admin/columns.php
@@ -4,7 +4,9 @@ if(!function_exists('wpc_manage_post_ad_columns')){
     function wpc_manage_post_ad_columns($columns)
     {
         $columns['wpc_views_column'] = __('Views', 'wpcourse');
-        $columns['wpc_thumbnail_column'] = __('Thumbnail','wpcourse');
+        if(current_Theme_supports('post-thumbnails')){
+            $columns['wpc_thumbnail_column'] = __('Thumbnail','wpcourse');
+        }
         return $columns;
     }
     add_filter('manage_post_posts_columns','wpc_manage_post_ad_columns');
