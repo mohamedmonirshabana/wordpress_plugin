0a2a7ee362de703c47e5078704cd12afada2cdb0
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-12 19:45:22 +0300

    add submenu for main menu in admin page

--- admin/custom-pages.php
+++ admin/custom-pages.php
@@ -8,5 +8,24 @@ add_action('admin_menu',function(){
         'wpc_options',
         'wpc_social_media_options',
         'dashicons-admin-generic',
+        78
+    );
+    add_submenu_page(
+        'wpc_options',
+        __('Social Media', 'wpcourse'),
+        __('Social Media', 'wpcourse'),
+        'manage_options',
+        'wpc_options',
+        'wpc_social_media_options',
+        1
+    );
+    add_submenu_page(
+        'wpc_options',
+        __('Post Options', 'wpcourse'),
+        __('Post Options', 'wpcourse'),
+        'manage_options',
+        'wpc_post_options',
+        'wpc_post_options',
+        2
     );
 });
