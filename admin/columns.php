64de978eae6f604384bb2e006c7a6a77fc15960d
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-12 17:12:53 +0300

    add code for columns show thumbnail and views

--- admin/columns.php
+++ admin/columns.php
@@ -1,11 +1,28 @@
 <?php 
 
 if(!function_exists('wpc_manage_post_ad_columns')){
-    function wpc_manage_post_ad_columns($columns){
+    function wpc_manage_post_ad_columns($columns)
+    {
         $columns['wpc_views_column'] = __('Views', 'wpcourse');
         $columns['wpc_thumbnail_column'] = __('Thumbnail','wpcourse');
         return $columns;
     }
     add_filter('manage_post_posts_columns','wpc_manage_post_ad_columns');
     add_filter('manage_wpc_ad_posts_columns','wpc_manage_post_ad_columns');
+}
+
+if(!function_exists('wpc_set_post_ad_column_content')){
+    function wpc_set_post_ad_column_content($column, $post_id)
+    {
+        switch($column){
+            case 'wpc_views_column':
+                echo ((int)(get_post_meta($post_id, 'wpc_post_views', true)));
+                break;
+            case 'wpc_thumbnail_column':
+                echo get_the_post_thumbnail($post_id, [100,100]);
+                break;
+        }
+    }
+    add_filter('manage_post_posts_custom_column', 'wpc_set_post_ad_column_content',10, 2);
+    add_filter('manage_wpc_ad_posts_custom_column', 'wpc_set_post_ad_column_content', 10 , 2);
 }
