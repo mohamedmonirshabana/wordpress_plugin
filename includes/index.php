b1798ac64bbbc3741d48a8de9a7719226ff6cbb0
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-19 18:27:29 +0300

    add link for shortcodes

--- includes/index.php
+++ includes/index.php
@@ -5,3 +5,4 @@ require plugin_dir_path(__FILE__) . 'custom-taxonomies.php';
 require plugin_dir_path(__FILE__) . 'metaboxes.php';
 require plugin_dir_path(__FILE__) . 'taxonomies-fields.php';
 require plugin_dir_path(__FILE__) . 'users.php';
+require plugin_dir_path(__FILE__) . 'shortcodes.php';
