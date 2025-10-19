922b00c57f5b684a0d8f43b72a1278ad1e3aabee
Mohamed Shabana <mohamed.monir.shabanan@gmail.com>
2025-10-19 19:12:49 +0300

    add code  to return ads

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,5 +1,19 @@
 <?php 
 
 add_shortcode('wpc_ad', function(){
-    return 'shortcode Content';
+    $ads = get_posts([
+        'post_type' => ['wpc_ad'],
+        'numberposts' => 1,
+        'orderby' =>  'rand',
+    ]);
+    if(count($ads)){
+        $result = '<div class="d-flex">';
+        $result .= '<div class="ad-thumbnail">' .get_the_post_thumbnail($ads[0]->ID, [110, 110]) .'</div>';
+        $result .= '<div class="ad-info">';
+        $result .= '<h6>'.$ads[0]->post_title.'</h6>';
+        $result .= '<p>'.$ads[0]->post_excerpt.'</p>';
+        $result .= '</div>';
+        $result .= '</div>';
+        return $result;
+    }
 });
